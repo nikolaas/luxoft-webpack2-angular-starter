c47eb26e2b583aed10751d75d9f92242417dbadc
Daniil Stepanov <sda.isu@gmail.com>
2017-03-01 13:14:54 +0600

    dev::LW-1::App component's name has been changed. (for testing purposes)

--- app/components/app/index.js
+++ app/components/app/index.js
@@ -1,6 +1,6 @@
 import angular from 'angular';
 import app from './app.component';
 
-export default angular.module('appComponent', [])
+export default angular.module('app-component', [])
     .component(app.name, app.config)
     .name;
